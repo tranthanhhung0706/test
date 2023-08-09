a618c54823b2a1f141266062468345c339d8741f
tranthanhhung0706 <67146014+tranthanhhung0706@users.noreply.github.com>
2023-08-09 14:35:07 +0700

    Update code with new feature

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-require("dotenv").config();
 const express = require("express")
-//import express from "express";
-import bodyParser  from "body-parser";
-import viewEngine from "./config/viewEngine";
-import initWebRoutes from "./routes/web";
-
+require('dotenv').config()
 const app = express()
-viewEngine(app);
+const port = process.env.PORT || 3000
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-initWebRoutes(app);
-let port =process.env.PORT||5000;
-app.listen(port,()=>{
-    console.log("Connect to port 5000");
+// Page Home
+app.get("/", (req, res) => {
+    res.send('SERVER ON1')
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Start server listen at http://localhost:${port}`)
 });
