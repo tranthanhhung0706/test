7625530c4c010e702d260b3b1be29c7ec0e6622e
tranthanhhung0706 <67146014+tranthanhhung0706@users.noreply.github.com>
2023-08-09 13:20:19 +0700

    Update code with new feature

--- index.js
+++ index.js
@@ -10,14 +10,7 @@ app.get("/", (req, res) => {
     res.send('SERVER ON')
 })
 
-// ZingMp3Router
-const ZingMp3Router = require("./src/routes/ZingRouter")
-app.use("/api", cors({ origin: '*' }), ZingMp3Router)
 
-// Page Error
-app.get("*", (req, res) => {
-    res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
-});
 
 app.listen(port, () => {
     console.log(`Start server listen at http://localhost:${port}`)
