a72dd881bcd98015a3208b543a5fa2713b267c5f
tranthanhhung0706 <67146014+tranthanhhung0706@users.noreply.github.com>
2023-08-10 09:48:18 +0700

    ko

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 // Page Home
 app.get("/", (req, res) => {
